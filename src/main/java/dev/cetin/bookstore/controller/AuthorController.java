94aa13fe1d8188a29844625d4952b46e61a9642d
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-03 21:09:44 +0400

    Add Author controller

--- src/main/java/dev/cetin/bookstore/controller/AuthorController.java
+++ src/main/java/dev/cetin/bookstore/controller/AuthorController.java
@@ -1,4 +1,60 @@
 package dev.cetin.bookstore.controller;
 
+import dev.cetin.bookstore.dto.AuthorRequest;
+import dev.cetin.bookstore.dto.AuthorResponse;
+import dev.cetin.bookstore.entity.Author;
+import dev.cetin.bookstore.entity.Book;
+import dev.cetin.bookstore.service.AuthorService;
+import dev.cetin.bookstore.service.BookService;
+import dev.cetin.bookstore.util.Converter;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/author")
+@AllArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
+    private final BookService bookService;
+
+    @GetMapping
+    public List<AuthorResponse> findAll() {
+        List<Author> authors = authorService.findAll();
+        return Converter.authorResponseConvert(authors);
+    }
+
+    @GetMapping("/{id}")
+    public AuthorResponse findById(@PathVariable("id") Long id) {
+        Author foundAuthor = authorService.findById(id);
+        return Converter.authorResponseConvert(foundAuthor);
+    }
+
+    @Transactional
+    @PostMapping
+    public AuthorResponse save(@Validated @RequestBody AuthorRequest authorRequest) {
+        Author foundAuthor = authorRequest.getAuthor();
+        authorService.save(foundAuthor);
+        return Converter.authorResponseConvert(foundAuthor);
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public AuthorResponse update(@PathVariable("id") Long id, @Validated @RequestBody AuthorRequest authorRequest) {
+        Author foundAuthor = authorRequest.getAuthor();
+        authorService.save(foundAuthor);
+        return Converter.authorResponseConvert(foundAuthor);
+    }
+    @Transactional
+    @DeleteMapping("/{id}")
+    public AuthorResponse delete(@PathVariable("id") Long id) {
+        Author foundAuthor = authorService.findById(id);
+        authorService.delete(id);
+        return Converter.authorResponseConvert(foundAuthor);
+    }
+
 }
--- src/main/java/dev/cetin/bookstore/util/Converter.java
+++ src/main/java/dev/cetin/bookstore/util/Converter.java
@@ -1,6 +1,8 @@
 package dev.cetin.bookstore.util;
 
+import dev.cetin.bookstore.dto.AuthorResponse;
 import dev.cetin.bookstore.dto.BookResponse;
+import dev.cetin.bookstore.entity.Author;
 import dev.cetin.bookstore.entity.Book;
 
 import java.util.ArrayList;
@@ -17,5 +19,16 @@ public class Converter {
 
     public static BookResponse bookResponseConvert(Book book) {
         return new BookResponse(book.getId(),book.getTitle(), book.getGenre(),book.getPublishDate(), book.getAuthor());
+    }
+
+    public static List<AuthorResponse> authorResponseConvert(List<Author> authors) {
+        List<AuthorResponse> authorResponses = new ArrayList<>();
+        for (Author author: authors) {
+            authorResponses.add(new AuthorResponse(author.getId(), author.getFirstName(), author.getLastName(), author.getBirthDate(),author.getBooks()));
+        }
+        return authorResponses;
+    }
+    public static AuthorResponse authorResponseConvert(Author author) {
+        return new AuthorResponse(author.getId(), author.getFirstName(), author.getLastName(), author.getBirthDate(),author.getBooks());
     }
 }
